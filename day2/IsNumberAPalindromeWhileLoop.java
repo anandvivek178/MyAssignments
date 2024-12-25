6a829cf646a62531c19158d0a6b344ea28924aa8
anandvivek178 <40720824+anandvivek178@users.noreply.github.com>
2024-12-25 20:38:53 +0530

    Update and rename IsNumberAPalindrome.java to IsNumberAPalindromeWhileLoop.java

--- day2/IsNumberAPalindromeWhileLoop.java
+++ day2/IsNumberAPalindromeWhileLoop.java
@@ -1,7 +1,7 @@
 package week1.day2;
 import java.util.Scanner;
 
-public class IsNumberAPalindrome {
+public class IsNumberAPalindromeWhileLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
