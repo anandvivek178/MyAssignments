c09c0d572188bbab90b0889ebc834db3a44a0fad
anandvivek178 <40720824+anandvivek178@users.noreply.github.com>
2024-12-29 21:46:13 +0530

    Add files via upload

---
+++ week2/day1/Auto.java
@@ -0,0 +1,12 @@
+package week2.day1;
+
+public class Auto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car carActions=new Car();
+		carActions.applyBrake();
+		carActions.wearSeatBelt();
+	}
+
+}
---
+++ week2/day2/LoginFunctionality.java
@@ -0,0 +1,27 @@
+package week2.day2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LoginFunctionality {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+		driver.findElement(By.className("decorativeSubmit")).click();
+		
+		driver.quit();
+		
+	}
+
+}
